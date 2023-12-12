82fd009146f86adb9dda6c68fccc43506dcf6f07
Bruno Lopes <bruno@bemobile.tech>
2023-12-12 15:59:14 -0300

    perf: optimize and improve the code

---
+++ BeBoasPraticas_02-03.js
@@ -0,0 +1,30 @@
+const API_KEY = "YOUR_API_KEY";
+const API_ENDPOINT = "https://api.example.com";
+
+function callAPI() {
+    fetch(`${API_ENDPOINT}/data?key=${API_KEY}`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+}
+callAPI();
+
+function calculateTotal(p, q) {
+    return p.reduce((total, p, index) => total + p * q[index], 0);
+}
+
+function calculate(p, q) {
+    let result = 0;
+
+    for (let i = 0; i <= p; i++) {
+    result += q[i];
+
+    }
+
+    return result;
+}
+
+function calculateDiscount(price) {
+    const PERCENTAGE_DISCOUNT = 0.1;
+    
+    return price * PERCENTAGE_DISCOUNT * price;
+}
